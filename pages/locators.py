788f891cc42a460bddbd0958eaf6a3fd4c0daa55
gshelukhin <shelukhin@restream.rt.ru>
2019-10-23 10:25:19 +0300

    Add login page locators and asserts

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,15 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.ID, "login_form")
+    REG_FORM = (By.ID, "register_form")
+    EMAIL_LOGIN_FIELD = (By.ID, "id_login-username")
+    PASSWORD_LOGIN_FIELD = (By.ID, "id_login-password")
+    EMAIL_REG_FIELD = (By.ID, "id_registration-email")
+    PASSWORD_REG_FIELD = (By.ID, "id_registration-password1")
+    PASSWORD_CONFIRM_REG_FIELD = (By.ID, "id_registration-password2")
+    LOGIN_BUTTON = (By.NAME, "login_submit")
+    REG_BUTTON = (By.NAME, "registration_submit")
